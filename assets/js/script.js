ca9cb0ac9f43bf63aa7e500e759b4b3b4278cbce
max <max@ubuntu.ubuntu.local>
2023-02-23 14:53:16 -0500

    adds getNOAATideData function which console logs tide data successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // DATA / STATE / GLOBAL VARIABLES
 // FUNCTIONS
 function loadPinnedBeaches() {
-  console.log("loading pinned beaches");
+  console.log('loading pinned beaches');
 }
 
 function populateBeachData(beach) {
@@ -13,12 +13,45 @@ function findBeaches(search) {
   console.log(`Searching for ${search}`);
 }
 
+function getNOAATideData(stationId = '1612340') {
+  // https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20250801&end_date=20250831&station=8557863&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&application=DataAPI_Sample&format=json
+  var baseURL = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?';
+  var paramsObject = {
+    date: 'today',
+    station: stationId,
+    product: 'predictions',
+    datum: 'MTL',
+    time_zone: 'lst_ldt',
+    interval: 'hilo',
+    units: 'english',
+    application: 'DataAPI_Sample',
+    format: 'json',
+  };
+
+  var requestURL = baseURL;
+
+  // for-of is for ARRAYS and for-in is for OBJECTS
+  for (var key of Object.keys(paramsObject)) {
+    requestURL += `${key}=${paramsObject[key]}&`;
+  }
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+getNOAATideData();
+
 function handlePinClick() {}
 // USER INTERACTIONS
 // a user submits the search form
-searchForm.addEventListener("submit", handleSubmitSearch);
+searchForm.addEventListener('submit', handleSubmitSearch);
 // a user clicks a pinned beach or search result
-beachButton.addEventListener("click", handlePinClick);
+beachButton.addEventListener('click', handlePinClick);
 // a user clickes data div
 // expands/collapse
 // a user clickes a search result
@@ -27,5 +60,5 @@ beachButton.addEventListener("click", handlePinClick);
 // load pinned beaches
 loadPinnedBeaches();
 // load initial beach
-populateBeachData({ name: "Bondi Beach" });
+populateBeachData({ name: 'Bondi Beach' });
 // default or last searched
